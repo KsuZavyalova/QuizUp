dd6fd29de2efaf42c8a983ae1195ccf38411f74e
KsuZavyalova <113120321+KsuZavyalova@users.noreply.github.com>
2024-10-18 01:11:58 +0300

    Update models.py

--- models.py
+++ models.py
@@ -4,14 +4,21 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
 class Poll(db.Model):
+    __tablename__ = 'polls'
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(200), nullable=False)
-    options = db.relationship('Option', backref='poll', lazy=True)
-
+    options = db.relationship('Option', backref='poll', lazy=True, cascade="all, delete-orphan")
 
 class Option(db.Model):
+    __tablename__ = 'options'
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(100), nullable=False)
-    poll_id = db.Column(db.Integer, db.ForeignKey('poll.id'), nullable=False)
+    poll_id = db.Column(db.Integer, db.ForeignKey('polls.id'), nullable=False)
     votes = db.Column(db.Integer, default=0)
